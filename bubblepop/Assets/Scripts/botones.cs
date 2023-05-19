0e719ca074f24603b78eb65f2bc2a8965742cef1
s/c <47247552+sofiacastaneda@users.noreply.github.com>
2023-05-18 23:41:26 -0500

    update

--- bubblepop/Assets/Scripts/botones.cs
+++ bubblepop/Assets/Scripts/botones.cs
@@ -7,24 +7,44 @@ using TMPro;
 public class botones : MonoBehaviour
 
 {
+    private GameObject showBubblesModo1Obj;
+    private GameObject showBubblesModo2Obj;
+    private GameObject showBubblesModo3Obj;
+
+    void Start()
+    {
+        GameObject showBubblesModo1Obj = GameObject.Find("Modo1");
+        GameObject showBubblesModo2Obj = GameObject.Find("Modo2");
+        GameObject showBubblesModo3Obj = GameObject.Find("Modo3");
+
+        showBubblesModo1Obj.SetActive(false);
+        showBubblesModo2Obj.SetActive(false);
+        showBubblesModo3Obj.SetActive(false);
+
+    }
 
     public void botonA() {
+
+        showBubblesModo1Obj.SetActive(true);
+        showBubblesModo2Obj.SetActive(false);
+        showBubblesModo3Obj.SetActive(false);
 
     }
 
     public void botonB()
     {
+        showBubblesModo1Obj.SetActive(false);
+        showBubblesModo2Obj.SetActive(true);
+        showBubblesModo3Obj.SetActive(false);
 
     }
 
     public void botonC()
     {
+        showBubblesModo1Obj.SetActive(false);
+        showBubblesModo2Obj.SetActive(false);
+        showBubblesModo3Obj.SetActive(true);
 
-    }
-
-    void Start()
-    {
-        
     }
 
     void Update()
