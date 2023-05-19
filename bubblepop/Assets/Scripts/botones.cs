c487838231ae0a2b17aa541d7c6021b52d2a5c44
s/c <47247552+sofiacastaneda@users.noreply.github.com>
2023-05-18 19:08:45 -0500

    update

---
+++ bubblepop/Assets/Scripts/botones.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class botones : MonoBehaviour
+
+{
+
+    public void botonA() {
+
+    }
+
+    public void botonB()
+    {
+
+    }
+
+    public void botonC()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
